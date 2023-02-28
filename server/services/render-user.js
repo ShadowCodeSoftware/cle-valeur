7bc4fab20babd31cec8edbe51cec670536ec486d
@Baro <ngayapjoel3@email.com>
2023-02-28 23:10:15 +0100

    kn,

--- server/services/render-user.js
+++ server/services/render-user.js
@@ -1,3 +1,8 @@
 exports.user = (req, res) => {
     res.render("screens/authentification/user_account");
 }
+
+
+exports.user = (req, res) => {
+    res.render("screens/authentification/user_account");
+}
