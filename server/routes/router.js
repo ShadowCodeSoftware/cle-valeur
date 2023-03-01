0521655fc9bc64b762962473a57b4ea15d754643
djin21 <johniannis.c@gmail.com>
2023-03-01 02:56:07 +0100

    Clean fournisseur routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const route = express.Router();
 
-const controller_four = require('../controller/controller-fournisseur');
-
-//Required
-const fournisseurServices = require('../services/render-fournisseur');
-
 const controllerClient = require('../controller/controller_client')
 const controller_vendeur = require('../controller/controller_vendeur')
 const controller_user = require("../controller/controller-user");
@@ -23,10 +18,8 @@ route.get('/', (req, res) => {
 require("./routes_articles")(route);
 
 
-//Fournisseur
-route.get('/fournisseur/add', fournisseurServices.add_fournisseur);
-route.get('/fournisseur/update/:id', fournisseurServices.show_data);
-// route.get('/fournisseur/update', fournisseurServices.show_data);
+// --- Fournisseur ---
+require("./routes-fournisseur")(route);
 
 // Client
 route.get('/clients/add',clientServices.add_client);
@@ -39,19 +32,6 @@ route.get('/sell/new', sellServices.new_sell);
 route.get('/sell/veiw/:id', controller_vendeur.findAllSellDetails);
 
 // API
-
-    
-//Fournisseur
-route.post('/api/fournisseur', controller_four.create);
-route.post('/api/fournisseur/maj', controller_four.update);
-
-route.get('/api/fournisseur', controller_four.find);
-route.get('/api/fournisseur/del/:id', controller_four.delete);
-
-route.get('/api/fournisseur/fetch/:id', controller_four.findOne);
-
-// route.put('/api/fournisseur/:id', controller_four.update);
-// route.delete('/api/fournisseur/:id', controller_four.delete);
 
 route.post('/api/sells', controller_vendeur.create);
 route.get('/api/sells', controller_vendeur.findAllSells);
