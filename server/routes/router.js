48cd710883f24a120a1e9d4a4b625853deea8da2
@Baro <ngayapjoel3@email.com>
2023-03-01 01:35:20 +0100

    commit

--- server/routes/router.js
+++ server/routes/router.js
@@ -53,5 +53,6 @@ route.get("/update_user", userServices.update);
 // API users
 route.post("/api/user", controller_user.create);
 route.get("/api/user", controller_user.findAll);
+route.post("/api/user", controller_user.update);
 
 module.exports = route
